7ea3dc76f6e0f174093000fd25d68b41206a2972
Cefan Rubin <cefan@rubin.za.net>
2021-04-02 21:52:53 +0200

    updated stop script too

--- stop.sh
+++ stop.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-SCRIPT_DIR=$(readlink -f $(dirname $0))
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 pushd $SCRIPT_DIR > /dev/null
 
-echo "purrs minikube profile being deleted..."
-./lib/mkube.sh profile delete purrs
+echo "purrs minikube profile being stopped..."
+bin/minikube stop
 
 popd > /dev/null
